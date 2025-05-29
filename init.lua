3eb5a0a4253479f0fc4e0501d6581e0633ab6baa
sr8e <sr@mell0w-5phere.net>
2025-05-29 17:29:25 +0900

    set more options

--- init.lua
+++ init.lua
@@ -3,14 +3,30 @@ vim.g.loaded_netrwPlugin = 1
 
 vim.opt.termguicolors = true
 vim.opt.number = true
+vim.opt.title = true
 
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
 vim.opt.ignorecase = true
+vim.opt.smartcase = true
 vim.opt.cursorline = true
 
+vim.opt.backspace = 'indent,eol,start'
+
+vim.opt.list = true
+vim.opt.listchars = {
+    tab = "»-",
+    space = '⸱',
+    multispace = space,
+    trail = space,
+    lead = space,
+    leadmultispace = space,
+    nbsp = space,
+}
+vim.cmd([[match TrailingSpaces /\s\+$/]])
+vim.api.nvim_set_hl(0, "TrailingSpaces", { link = "ErrorMsg"})
 
 --clipboard
 vim.opt.clipboard = "unnamedplus"
