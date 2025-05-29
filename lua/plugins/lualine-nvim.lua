5d2eb99227338ec570dba783f5fd7bc00e8b9ac0
sr8e <sr@mell0w-5phere.net>
2025-05-29 17:29:45 +0900

    change tabline colors

--- lua/plugins/lualine-nvim.lua
+++ lua/plugins/lualine-nvim.lua
@@ -1,3 +1,13 @@
+local colors = {
+    black        = '#202020',
+    neon         = '#DFFF00',
+    white        = '#FFFFFF',
+    gray         = '#444444',
+    darkgray     = '#3c3836',
+    lightgray    = '#504945',
+    inactivegray = '#7c6f64',
+}
+
 return {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
@@ -10,7 +20,16 @@ return {
             lualine_a = {
                 {
                     'buffers',
-                    buffers_color = switch_color,
+                    buffers_color = {
+                        active = {
+                            bg = colors.neon,
+                            fg = colors.black,
+                        },
+                        inactive = {
+                            bg = colors.darkgray,
+                            fg = colors.inactivegray,
+                        },
+                    },
                     symbols = { modified = '[+]'},
                 },
             },
