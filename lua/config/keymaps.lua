0e0f5167897dd60014c2deb6a2faa446350b4a93
sr8e <sr@mell0w-5phere.net>
2025-05-30 17:24:34 +0900

    add more keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -10,34 +10,30 @@ vim.keymap.set({'n', 'i',}, '<Right>', '<nop>')
 --insert mode
 vim.keymap.set('i', 'jj', '<esc>')
 vim.keymap.set('i', 'ｊｊ', '<esc>') -- ?
-vim.keymap.set('i', '<C-h>', '<Left>')
+vim.keymap.set('i', '<C-h>', '<Left>') -- move cursor in insert mode
 vim.keymap.set('i', '<C-j>', '<Down>')
 vim.keymap.set('i', '<C-k>', '<Up>')
 vim.keymap.set('i', '<C-l>', '<Right>')
 
 --normal mode
--- wrapped lines
-vim.keymap.set('n', 'j', 'gj')
+vim.keymap.set('n', 'j', 'gj') -- wrapped lines
 vim.keymap.set('n', 'k', 'gk')
--- anchors
-vim.keymap.set('n', '<leader>h', '^')
+vim.keymap.set('n', '<leader>h', '^') -- anchors
 vim.keymap.set('n', '<leader>l', '$')
---linebreaks
-vim.keymap.set('n', '<C-j>', 'i<CR><esc>')
--- paste from last yank
-vim.keymap.set('n', ',p', '"0p')
+vim.keymap.set('n', ',,', '%')
+vim.keymap.set('n', '<C-j>', 'i<CR><esc>') --linebreaks
+vim.keymap.set('n', ',p', '"0p') -- paste from last yank
 vim.keymap.set('n', ',P', '"0P')
---buffers
-vim.keymap.set('n', '<leader>bh', '<cmd>bp<CR>')
+vim.keymap.set('n', '<leader>bh', '<cmd>bp<CR>') --buffers
 vim.keymap.set('n', '<leader>bl', '<cmd>bn<CR>')
 vim.keymap.set('n', '<leader>bx', '<cmd>bd<CR>')
---panes
-vim.keymap.set('n', '<leader>ph', '<C-w>h')
+vim.keymap.set('n', '<leader>bX', '<cmd>bd!<CR>')
+vim.keymap.set('n', '<leader>ph', '<C-w>h') --panes
 vim.keymap.set('n', '<leader>pl', '<C-w>l')
---tabs
-vim.keymap.set('n', '<leader>th', '<cmd>tabp<CR>')
+vim.keymap.set('n', '<leader>th', '<cmd>tabp<CR>') --tabs
 vim.keymap.set('n', '<leader>tl', '<cmd>tabn<CR>')
 vim.keymap.set('n', '<leader>tx', '<cmd>tabclose<CR>')
+
 --nvim-tree
 vim.keymap.set('n', '<leader>tt', '<cmd>NvimTreeToggle<CR>')
 vim.keymap.set('n', '<leader>to', '<cmd>NvimTreeOpen<CR>')
